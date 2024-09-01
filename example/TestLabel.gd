fb42d6ede9d4bf24529750ab14b93a06b6e824a1
Victoria Solli <10584311+Bikutoso@users.noreply.github.com>
2024-09-01 04:18:54 +0200

    Comment example for better clarity for usage

--- example/TestLabel.gd
+++ example/TestLabel.gd
@@ -3,8 +3,10 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  GlobalSignal.add_listener('text_updated', self, '_on_Text_updated')
+	# Add a listener that will call "_on_text_updated" when a signal is
+	# emited with "text_updated"
+	GlobalSignal.add_listener('text_updated', self, '_on_text_updated')
 
 
-func _on_Text_updated(text_value: String):
-  text = text_value
+func _on_text_updated(text_value: String):
+	text = text_value
--- example/TestTextEdit.gd
+++ example/TestTextEdit.gd
@@ -5,10 +5,14 @@ signal text_updated(text_value)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  GlobalSignal.add_emitter('text_updated', self)
-  GlobalSignal.emit_signal_when_ready('text_updated', ['text in _ready()'], self)
-  connect('text_changed',Callable(self,'_on_Text_changed'))
+	# text_updated is added as a global signal
+	GlobalSignal.add_emitter('text_updated', self)
+	# When the node is ready emmit the string "Hello, World!" and update the label
+	GlobalSignal.emit_signal_when_ready('text_updated', ['Hello, World!'], self)
+	# Update the label every time the text changes in the text window
+	connect('text_changed',Callable(self,'_on_text_changed'))
 
 
-func _on_Text_changed(_value):
-  emit_signal('text_updated', text)
+func _on_text_changed(_value):
+	# Emmit the signal in a normal godot 4.x manner
+	text_updated.emit(text)
